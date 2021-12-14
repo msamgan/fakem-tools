c80524021ab27aad2853f1fd171206d55d8a2e84
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2021-12-14 17:14:53 +0400

    Inner pages updated

--- src/pages/ContentGenerator/ContentGenerator.js
+++ src/pages/ContentGenerator/ContentGenerator.js
@@ -23,27 +23,30 @@ const ContentGenerator = () => {
     }, [])
 
     return <MasterLayout>
-        <h1 className={'mt-4'}># Content Generator</h1>
-        <p className={'text-muted'}>Random generated content for and as per your
-            requirement.</p>
-        <div className={'row mt-3'}>
-            <div className={'col-md-2'}>
-                <button className={'btn btn-primary'}
-                        disabled={generateNewDisable} onClick={async () => {
-                    setGenerateNewDisable(true)
-                    await getRandomContent()
-                    setGenerateNewDisable(false)
-                }}>Generate
-                </button>
+        <div className={'container mb-3 mt-5 min-height'}>
+            <h1 className={'mt-4'}># Content Generator</h1>
+            <p className={'text-muted'}>Random generated content for and as per
+                your
+                requirement.</p>
+            <div className={'row mt-3'}>
+                <div className={'col-md-2'}>
+                    <button className={'btn btn-primary'}
+                            disabled={generateNewDisable} onClick={async () => {
+                        setGenerateNewDisable(true)
+                        await getRandomContent()
+                        setGenerateNewDisable(false)
+                    }}>Generate
+                    </button>
+                </div>
             </div>
-        </div>
-        <div className={'row mt-3'}>
-            <div className={'col-md-12'}>
-                {
-                    !isEmpty(content) ? Object.keys(content).map((key, i) =>
-                        <p>{content[key]}</p>
-                    ) : <p>Getting a freshly baked content for you....</p>
-                }
+            <div className={'row mt-3'}>
+                <div className={'col-md-12'}>
+                    {
+                        !isEmpty(content) ? Object.keys(content).map((key, i) =>
+                            <p>{content[key]}</p>
+                        ) : <p>Getting a freshly baked content for you....</p>
+                    }
+                </div>
             </div>
         </div>
     </MasterLayout>
