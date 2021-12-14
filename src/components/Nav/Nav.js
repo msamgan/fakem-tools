44770a1110a32f7cf772a34d78a49db45c6d2221
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2021-12-14 17:10:45 +0400

    added icons n grif

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,55 +7,59 @@ import logo from '../../assets/logo-fakem.svg'
 const Nav = () => {
     let history = useHistory();
 
-    return <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid">
+    return <div className={'container'}>
+        <nav className="navbar navbar-expand-lg navbar-light">
             <Link className="navbar-brand" to={'/'}>
-                <img src={logo} alt={'fakem-log'}/>
-                <span className={'text-muted'}>Fakem Tools</span>
+                <img src={logo} alt={'fakem-log'} style={{
+                    height: '28px'
+                }}/>
+                <span className={'text-muted'} style={{marginLeft: '10px'}}>Fakem Tools</span>
             </Link>
-            <button className="navbar-toggler" type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse"
-                 id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    {/*<li className="nav-item">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse"
+                     id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {/*<li className="nav-item">
                         <Link className="nav-link" to={'/'}
                               aria-current="page">Home</Link>
                     </li>*/}
-                    <li className="nav-item">
-                        <Link className="nav-link"
-                              to={'/user'}>User</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link"
-                              to={'/content'}>Content</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link"
-                              to={'/api'}>Test Api's</Link>
-                    </li>
-                </ul>
-                <form className="d-flex">
-                    <Searchable
-                        value=""
-                        placeholder="Search for the tool"
-                        notFoundText="No result found"
-                        options={menu}
-                        onSelect={value => {
-                            return history.push(value)
-                        }}
-                        listMaxHeight={200}
-                    />
-                </form>
+                        <li className="nav-item">
+                            <Link className="nav-link"
+                                  to={'/user'}>User</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link"
+                                  to={'/content'}>Content</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link"
+                                  to={'/api'}>Test Api's</Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <Searchable
+                            value=""
+                            placeholder="Search for the tool"
+                            notFoundText="No result found"
+                            options={menu}
+                            onSelect={value => {
+                                return history.push(value)
+                            }}
+                            listMaxHeight={200}
+                        />
+                    </form>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 }
 
 export default Nav;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MasterLayout from "../../layout/MasterLayout/MasterLayout";
 import Searchable from 'react-searchable-dropdown';
-import {useHistory} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import menu from '../../components/Nav/menu.json'
 
 
@@ -9,38 +9,80 @@ const HomePage = () => {
     let history = useHistory();
 
     return <MasterLayout>
-        <h1 className={'mt-5'}>Fakem Tools</h1>
-        <p>
-            Tools created by the developers for the developers, and yes its all
-            open source...
-        </p>
 
-        <p>
-            Fakem is a open source fake user generator which is the most easy to
-            use user generator
-            available out there.
-            <br/>
-            It also provide you with ready to use temporary email inbox,
-            available at just one click.
-            Generate names, addresses, about content etc, for absolute free of
-            any charge.
-        </p>
+        <div className={'container'}>
+            <div className={'row mt-5'}>
+                <div className={'col-md-4'}>
+                    <h5>
+                        Tools created by the developers for the developers, and
+                        yes
+                        its all
+                        open source...
+                    </h5>
+                </div>
+                <div className={'col-md-8'}>
+                    <p>
+                        Fakem is a open source fake user generator which is the
+                        most
+                        easy to
+                        use user generator
+                        available out there.
+                        <br/>
+                        It also provide you with ready to use temporary email
+                        inbox,
+                        available at just one click.
+                        Generate names, addresses, about content etc, for
+                        absolute
+                        free of
+                        any charge.
+                    </p>
 
-        <p>
-            Providing a number a tools for developers required in day to day
-            development.
-        </p>
-        <h4 className={'mb-3'}>Search for required tool</h4>
-        <Searchable
-            value=""
-            placeholder="Search for the tool"
-            notFoundText="No result found"
-            options={menu}
-            onSelect={value => {
-                return history.push(value)
-            }}
-            listMaxHeight={200}
-        />
+                    <p>
+                        Providing a number a tools for developers required in
+                        day to
+                        day
+                        development.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className={'bg-grey min-height'}>
+            <div className={'container mb-3 mt-5'}>
+                <h4 className={'pt-5'}>Search for required tool</h4>
+                <Searchable
+                    value=""
+                    placeholder="Search for the tool"
+                    notFoundText="No result found"
+                    options={menu}
+                    onSelect={value => {
+                        return history.push(value)
+                    }}
+                    listMaxHeight={200}
+                />
+
+                <div className={'row'}>
+                    {
+                        menu.map(item =>
+                            <div className={'col-md-3 mt-5'}>
+                                <Link to={item.value}>
+                                    <div className="card copy">
+                                        <img src={item.icon}
+                                             className="menu-icon mt-3"
+                                             alt={item.label}/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">{item.label}</h5>
+                                            <p className="card-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>)
+                    }
+                </div>
+
+            </div>
+        </div>
+
     </MasterLayout>
 }
 HomePage.propTypes = {};
